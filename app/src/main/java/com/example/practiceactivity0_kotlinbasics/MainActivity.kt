69cac8dde9ea67f2041881f2d2980fa28eb72599
Jubibani <strawberryloli3@gmail.com>
2024-09-17 13:18:52 +0800

    add conversion with random generation

---
+++ app/src/main/java/com/example/practiceactivity0_kotlinbasics/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.practiceactivity0_kotlinbasics
+
+import kotlin.random.Random
+
+fun main() {
+    // random pounds and convert to kilograms
+    val pounds = Random.nextInt(1, 300)  // Random weight between 1 and 300 pounds
+    println("Weight in Pounds: $pounds (lbs)")
+    val kilograms = pounds * 0.453592
+    println("Converted in Kilograms: $kilograms (kg)")
+    println("==================================")
+
+    // random miles and convert to kilometers
+    val miles = Random.nextInt(1, 500)
+    println("Length in Miles: $miles (mi)")
+    val kilometers = miles * 1.60934
+    println("Converted in Kilometers = $kilometers kilometers (km)")
+    println("==================================")
+
+    // random Fahrenheit and convert to Celsius
+    val fahrenheit = Random.nextInt(-50, 120)
+    println("Temperature from Fahrenheit: $fahrenheit (°F)")
+    val celsius = (fahrenheit - 32) * 5 / 9
+    println("Converted in Celsius = $celsius (°C)")
+    println("==================================")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Practice Activity 0 - Kotlin Basics"
+include(":app")
