e21efa4c9dad65dc7f94d16ac2918b9d75a2c0a7
Jubibani <strawberryloli3@gmail.com>
2024-09-17 23:02:10 +0800

    get the average age of 10 students

--- app/src/main/java/com/example/practiceactivity0_kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/practiceactivity0_kotlinbasics/MainActivity.kt
@@ -3,24 +3,41 @@ package com.example.practiceactivity0_kotlinbasics
 import kotlin.random.Random
 
 fun main() {
+    conversion()
+    averageAgeStudents()
+}
+fun conversion() {
     // random pounds and convert to kilograms
     val pounds = Random.nextInt(1, 300)  // Random weight between 1 and 300 pounds
-    println("Weight in Pounds: $pounds (lbs)")
+    println("Weight in Pounds (lbs): $pounds")
     val kilograms = pounds * 453592 / 1000000
-    println("Converted in Kilograms: $kilograms (kg)")
+    println("Weight converted in Kilograms (kg): $kilograms")
     println("==================================")
 
     // random miles and convert to kilometers
     val miles = Random.nextInt(1, 500)
-    println("Length in Miles: $miles (mi)")
+    println("Length in Miles (mi): $miles")
     val kilometers = miles * 160934 / 100000
-    println("Converted in Kilometers = $kilometers kilometers (km)")
+    println("Length converted in Kilometers (km): $kilometers kilometers")
     println("==================================")
 
     // random Fahrenheit and convert to Celsius
     val fahrenheit = Random.nextInt(-50, 120)
-    println("Temperature from Fahrenheit: $fahrenheit (°F)")
+    println("Temperature from Fahrenheit (°F): $fahrenheit")
     val celsius = (fahrenheit - 32) * 5 / 9
-    println("Converted in Celsius = $celsius (°C)")
+    println("Temperature converted in Celsius (°C): $celsius ")
+    println("==================================")
+}
+fun averageAgeStudents() {
+
+    val ages: List<Int> = List(10) {Random.nextInt(18,28) }
+
+    ages.forEachIndexed { index, age ->
+        println("Age of Student ${index + 1}: $age")
+    }
+
+    val age_average = ages.average()
+
+    println("The average age of the students is: $age_average")
     println("==================================")
 }
